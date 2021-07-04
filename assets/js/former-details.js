e9ff736d46b20182690eab18cc0262f3fbd898df
Tiafeno <tiafenofnel@gmail.com>
2021-07-04 14:57:38 +0300

    Add former details

--- assets/js/former-details.js
+++ assets/js/former-details.js
@@ -2,20 +2,101 @@
     $().ready(function () {
         const Layout = {
             template: '#layout',
-            delimiters: ['${', '}']
+            delimiters: ['${', '}'],
+            data: function() {
+                return {
+                    axiosInstance: null,
+                }
+            },
+            created: function () {
+                this.axiosInstance = axios.create({
+                    baseURL: apiSettings.ajaxurl,
+                    headers: {
+                        'X-WP-Nonce': apiSettings.nonce
+                    }
+                });
+            }
         };
-        const Details = {
-            template: '#details'
+        const FormerDetails = {
+            template: '#details',
+            delimiters: ['${', '}'],
+            data: function() {
+                return {
+                    axiosInstance: null,
+                    postNew: apiSettings.product_post_new,
+                    currency: apiSettings.currency,
+                    products: [],
+                }
+            },
+            mounted: function () {
+                var self = this;
+                var data = new FormData();
+                data.append('action', 'action_former_details');
+                data.append('former_id', apiSettings.former_id);
+                this.$parent.axiosInstance.post('', data).then(function(resp) {
+                    var response = lodash.clone(resp.data);
+                    if(response.success) {
+                        self.products = lodash.clone(response.data);
+                    }
+                });
+            }
         };
+        const TrainingDetails = {
+            template: '#training-details',
+            delimiters: ['${', '}'],
+            data: function() {
+                return {
+                    TotalTTC: 0,
+                    Product : null,
+                    currency: apiSettings.currency,
+                    items: [],
+                }
+            },
+            mounted: function () {
+                var self = this;
+                var product_id = this.$route.params.id;
+                var data = new FormData();
+                data.append('action', 'action_get_product_details');
+                data.append('product_id', product_id);
+                this.$parent.axiosInstance.post('', data).then(function(resp) {
+                    var response = lodash.clone(resp.data);
+                    if(response.success) {
+                        self.TotalTTC = lodash.sum(lodash.map(response.data, i => i.totalTTC));
+                        self.items = lodash.clone(response.data);
+                    }
+                });
+
+                var product = new wp.api.models.Product( {id: product_id });
+                product.fetch().done(function(prod) {
+                    self.Product = lodash.clone(prod);
+                });
+
+
+            },
+            created: function() {
+
+            }
+        };
+
         const routes = [
             {
                 path: '/',
                 component: Layout,
-                redirect: '/details',
+                redirect: '/former',
                 children: [
-                    {path: 'details', name: 'Details', component: Details},
+                    {
+                        path: 'former',
+                        name: 'FormerDetails',
+                        component: FormerDetails
+                    },
+                    {
+                        path: '/details/:id',
+                        name: 'TrainingDetails',
+                        component: TrainingDetails,
+                    }
                 ],
-            }
+            },
+
         ];
         var router = new VueRouter({
             routes // short for `routes: routes`
--- inc/former-details.php
+++ inc/former-details.php
@@ -10,11 +10,16 @@ final class FormerDetailsClass {
     public function __construct () {
         add_action('add_meta_boxes', [&$this, 'add_former_meta_boxes']);
         add_action('save_post', [&$this, 'save_former_details']);
-        add_action('admin_enqueue_scripts', [$this, 'enqueue_scripts']);
+        add_action('admin_init', function() {
+            add_action('wp_ajax_action_former_details', [&$this, 'get_former_trainings']);
+            add_action('wp_ajax_action_get_product_details', [&$this, 'get_product_details']);
+        });
+
     }
 
     public function add_former_meta_boxes() {
         if (post_type_exists('former')) {
+            add_action('admin_enqueue_scripts', [$this, 'enqueue_scripts']);
             add_meta_box(
                 'former-details',
                 __( 'Former Details', 'ticket-falicrea' ),
@@ -67,16 +72,120 @@ final class FormerDetailsClass {
     }
 
     public function enqueue_scripts() {
+        global $post;
         wp_register_script('axios', plugin_dir_url(__FILE__) . '../assets/js/axios.min.js', [], null, true);
         wp_register_script('vue', plugin_dir_url(__FILE__) . '../assets/js/vue.js', ['jquery'], null, true);
         wp_register_script('vue-router', plugin_dir_url(__FILE__) . '../assets/js/vue-router.js', ['vue'], null, true);
         wp_register_script('former-details', plugin_dir_url(__FILE__) . '../assets/js/former-details.js',
-            ['vue', 'vue-router', 'axios'], null, true);
+            ['vue', 'vue-router', 'axios', 'lodash', 'wp-api'], null, true);
         wp_localize_script('former-details', 'apiSettings', [
+            'ajaxurl' => admin_url('admin-ajax.php'),
+            'former_id' => $post->ID,
+            'currency' => get_woocommerce_currency_symbol(),
+            'product_post_new' => admin_url('post-new.php?post_type=product'),
             'nonce' => wp_create_nonce('wp_rest'),
-            'root' => esc_url_raw(rest_url())
         ]);
+
+        wp_enqueue_style('semantic-table', plugin_dir_url(__FILE__) . '../assets/css/table.css');
+        wp_enqueue_style('semantic-btn', plugin_dir_url(__FILE__) . '../assets/css/button.css');
+        wp_enqueue_style('semantic-header', plugin_dir_url(__FILE__) . '../assets/css/header.min.css');
+        wp_enqueue_style('semantic-icon', plugin_dir_url(__FILE__) . '../assets/css/icon.css');
     }
+
+    public function get_former_trainings() {
+        global $wpdb;
+        if (!isset($_POST['former_id'])) {
+            wp_send_json_error('param `former_id` not defined');
+        }
+        $former_id = intval($_POST['former_id']);
+        $responses = [];
+        $sql = "SELECT post.ID, post.post_title, meta.meta_value as former_id FROM {$wpdb->posts} as post 
+JOIN {$wpdb->postmeta} as meta ON (meta.post_id = post.ID)
+WHERE post.post_type = %s AND meta.meta_value LIKE %s AND meta.meta_key = %s";
+        $prepare = $wpdb->prepare($sql, 'product', $former_id, 'former' );
+        $products = $wpdb->get_results($prepare);
+        if (empty($products)) wp_send_json_success([]);
+        // Request params
+        $request = new WP_REST_Request();
+        $request->set_param('context', 'edit');
+        foreach ($products as $product) {
+            $product_controller = new WC_REST_Products_V1_Controller();
+            $product_id = intval($product->ID);
+            $product = wc_get_product($product_id);
+            $response = $product_controller->prepare_item_for_response( $product , $request);
+            $responses[] = $response->data;
+        }
+        $wpdb->flush();
+        wp_send_json_success($responses);
+    }
+
+    public function get_product_details() {
+        global $wpdb;
+        $RESPONSES = [];
+        if (!isset($_POST['product_id'])) {
+            wp_send_json_error('param `product_id` not defined');
+        }
+        $product_id = intval($_POST['product_id']);
+        // Define HERE the orders status to include in  <==  <==  <==  <==  <==  <==  <==
+        $orders_statuses = "'wc-completed', 'wc-processing', 'wc-on-hold'";
+
+        # Get All defined statuses Orders IDs for a defined product ID (or variation ID)
+        $results = $wpdb->get_col( "
+        SELECT DISTINCT woi.order_id
+        FROM {$wpdb->prefix}woocommerce_order_itemmeta as woim, 
+             {$wpdb->prefix}woocommerce_order_items as woi, 
+             {$wpdb->prefix}posts as p
+        WHERE  woi.order_item_id = woim.order_item_id
+        AND woi.order_id = p.ID
+        AND p.post_status IN ( $orders_statuses )
+        AND woim.meta_key IN ( '_product_id', '_variation_id' )
+        AND woim.meta_value LIKE '$product_id'
+        ORDER BY woi.order_item_id DESC");
+
+        if (empty($results)) wp_send_json_success([]);
+
+        // Request params
+        $request = new WP_REST_Request();
+        $request->set_param('context', 'edit');
+
+        // Get all order for his product
+        foreach ($results as $index => $order_id) {
+            $totalTTC = 0;
+            $order_id = intval($order_id);
+            $order = wc_get_order($order_id);
+
+            $items = $order->get_items();
+            foreach ( $items as $item ) {
+                $data = $item->get_data();
+                if (intval($product_id) !== intval($data['product_id'])) continue;
+                $totalTTC += ($data['total'] + $data['total_tax']);
+            }
+
+            $customer_id = $order->get_customer_id();
+            $customer_controller = new WC_REST_Customers_V1_Controller();
+            $customer_response = $customer_controller->prepare_item_for_response(new WC_Customer($customer_id), $request);
+
+            $RESPONSES[] = [
+                'totalTTC' => $totalTTC,
+                'code' => $this->get_order_ticket($order_id),
+                'order_url' => $order->get_edit_order_url(),
+                'customer' => $customer_response->data
+            ];
+        }
+
+        wp_send_json_success($RESPONSES);
+    }
+
+    private function get_order_ticket($order_id) {
+        global $wpdb;
+        $table = $wpdb->prefix . 'ticket_security';
+        $request = $wpdb->prepare("SELECT code FROM $table WHERE order_id = %d", intval($order_id));
+        $response = $wpdb->get_col($request);
+        $wpdb->flush();
+        if (empty($response) || !$response) return null;
+        return reset($response);
+    }
+
 }
 
 new FormerDetailsClass();
